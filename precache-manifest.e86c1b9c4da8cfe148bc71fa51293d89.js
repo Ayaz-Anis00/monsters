9f14357bf9224c1f1e7de98e7ffa1737ae523b90
Ayaz-Anis00 <58621127+Ayaz-Anis00@users.noreply.github.com>
2020-07-27 23:05:20 +0530

    Updates

---
+++ precache-manifest.e86c1b9c4da8cfe148bc71fa51293d89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb894735d383b56da99756cfbec4c3d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "8f8a206d60df0e5c0039",
+    "url": "/monsters/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "8f8a206d60df0e5c0039",
+    "url": "/monsters/static/js/main.a888db6f.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
